14ac0ebe5800d5dfb435ac0fab8b2e3f6ed08884
jinwon.yang <yjw2288@gmail.com>
2023-11-26 16:26:58 +0900

    트랜잭션 정리

--- db/db-core/src/main/java/com/demo/sample/SampleService.kt
+++ db/db-core/src/main/java/com/demo/sample/SampleService.kt
@@ -8,8 +8,11 @@ import org.springframework.transaction.annotation.Transactional
 class SampleService internal constructor(
     private val sampleRepository: SampleRepository
 ) {
+    @Transactional(readOnly = true)
     fun findAll(): List<Sample> = sampleRepository.findAll()
-    fun save(sample: Sample): Sample = sampleRepository.save(sample)
 
+    @Transactional(readOnly = true)
     fun findAllByName(name: String): List<Sample> = sampleRepository.findAll(QSample.sample.name.eq(name)).toList()
+
+    fun save(sample: Sample): Sample = sampleRepository.save(sample)
 }
